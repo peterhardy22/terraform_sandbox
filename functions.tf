7390beb62263dd279e40fd860d509073c215a22c
Peter Hardy <peterphardy@gmail.com>
2023-12-02 21:03:37 -0600

    Understanding what functions are available

---
+++ functions.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+    region = var.region
+    access_key = "abcdefghijklmnopqrstuvwxyz"
+    secret_key = "1234567890987654321"
+}
+
+locals {
+    time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+}
+
+variable "region" {
+    default = "us-east-2"
+}
+
+variable "tags" {
+    type = list
+    default = ["first_instance","second_instance"]
+}
+
+variable "ami" {
+    type = map
+    default = {
+        "us-east-2" = "ami-123456789"
+        "us-east-1" = "ami-987654321"
+        "us-west-1" = "ami-192837465"
+    }
+}
+
+resource "aws_key_pair" "loginkey" {
+    key_name = "login-key"
+    public_key = file("${path.module}/id_rsa.pub")
+}
+
+resource "aws_instance" "app-dev" {
+    ami = lookup(var.ami,var.region)
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.loginkey.key_name
+    count = 2
+
+    tags = {
+        Name = element(var.tags,count.index)
+    }
+}
